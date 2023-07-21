501199d750f9f6b6f145233dd8330d6e174e91a3
Iyanuoluwapo Adesanya <aiyanu1.00@gmail.com>
2023-07-21 22:18:13 +0100

    redux is not for the week switch to redux toolkit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import cartItems from "../cart-items";
+// redux stuff
+import { createStore } from "redux"
+import reducer from "../reducer";
+const initialStore = {
+    cart: cartItems,
+    total: 0,
+    amount: 0
+}
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+export const store = createStore(reducer, initialStore,window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__())
